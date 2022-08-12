52f195e4784dbdcd7d7f9d51c133ab3acd4f4d71
Paneru2191 <111098174+Paneru2191@users.noreply.github.com>
2022-08-12 06:36:32 +0545

    Add files via upload

---
+++ NUpCount.java
@@ -0,0 +1,20 @@
+public class NUpCount {
+    public static void main(String args[])
+    {
+        int upcount;
+        int a[]={2, 3, 1, -6, 8, -3 -1, 2};
+        upcount=isUpCount(a,5);
+        System.out.println(upcount);
+    }
+    public static int isUpCount(int[] a,int n)
+    {
+        int partialSum=0, nUpCount=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(partialSum<=n&&partialSum+a[i]>n)
+            nUpCount++;
+        }
+
+      return nUpCount;
+    }
+}
