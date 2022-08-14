cf5e1a31f7ab7631b9a508aa2833c2f958e5ece7
Paneru2191 <111098174+Paneru2191@users.noreply.github.com>
2022-08-14 20:16:06 +0545

    Add files via upload

---
+++ ISnUnique.java
@@ -0,0 +1,25 @@
+public class ISnUnique {
+   public static void main(String args[])
+   {
+    int a[]={7, 3, 3, 2, 4};
+    int result=isNUnique(a,11);
+    System.out.println(result);
+   } 
+   public static int isNUnique(int a[], int n)
+   {
+      int flag=0;
+      for(int i=0;i<a.length-1;i++)
+      {
+        for(int j=i+1;j<a.length;j++)
+        {
+          if(a[i]+a[j]==n)
+           flag++;
+           if(flag>1)
+           return 0;  
+        }
+      }
+      if(flag==1)
+      return 1;
+      else return 0;
+   }
+}
