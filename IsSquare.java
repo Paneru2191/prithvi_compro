44c89b6739adbc9215964f32671ee1ed5a64473c
Paneru2191 <111098174+Paneru2191@users.noreply.github.com>
2022-08-14 20:37:50 +0545

    Add files via upload

---
+++ IsSquare.java
@@ -0,0 +1,21 @@
+public class IsSquare {
+    public static void main(String args[])
+    {
+        int issquare=isSquare(0);
+        System.out.println(issquare);
+    }
+    public static int isSquare(int n)
+    {
+        
+        for(int i=0;i<=n;i++)
+        {
+            if(i*i==n)
+            {
+                return 1;
+                
+            }
+            
+        }
+        return 0;
+    }
+}
