00a50bb8fb1536530ba776dd9e59e1bde4dded08
Paneru2191 <111098174+Paneru2191@users.noreply.github.com>
2022-08-21 22:07:04 +0545

    Add files via upload

---
+++ IsSequencedArray.java
@@ -0,0 +1,28 @@
+public class IsSequencedArray {
+    public static void main(String args[])
+    {
+        int a[]={-5, -5, -4, -4, -4, -3, -3, -2, -2, -2};
+        int result=isSequencedArray(a,-5,-2);
+        System.out.println(result);
+    }
+    public static int isSequencedArray(int[] a,int m, int n)
+    {
+        if(a.length==0)
+        return 0;
+        if(a.length==1&&m!=n)
+        return 0;
+        if(a[0]!=m&&a[a.length-1]!=n)
+        return 0;
+        for(int i=0;i<a.length-1;i++)
+        {
+           if(a[i+1]<a[i])
+           return 0;
+           if(a[i]!=a[i+1])
+           {
+            if(a[i+1]!=a[i]+1)
+            return 0;
+           }
+        }
+        return 1;
+    }
+}
