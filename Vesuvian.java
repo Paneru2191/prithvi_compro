cb305701687fdb2d08fcb3c0a5d60f3add02f6ad
Paneru2191 <111098174+Paneru2191@users.noreply.github.com>
2022-11-17 20:27:22 +0545

    Update Vesuvian.java

--- Vesuvian.java
+++ Vesuvian.java
@@ -1,28 +1,20 @@
 public class Vesuvian {
     public static void main(String args[])
     {
-        System.out.println(isVesuvian(41));
+        System.out.println(isVesuvian(50));
     }
     public static int isVesuvian(int n)
-    {
-        for(int i=1;i<n;i++)
-        {    if(isSquare(i)==1){
-                for(int j=1;j<n;j++)
+    { int c=0;
+        for(int i=1;i*i<n;i++)
+        {    
+                for(int j=i;j*j<n;j++)
                 {
-                    if((isSquare(j)==1)&&(i+j==n))
-                    return 1;
+                    if(i*i+j*j==n)
+                    c++;
                 }
-            }    
+                
         }
-        return 0;
+        if(c==2) return 1; else return 0;
     }
-    public static int isSquare(int n)
-    {
-        for(int i=1;i<n;i++)
-        {
-            if(i*i==n)
-            return 1;
-        }
-        return 0;
-    }
+    
 }
