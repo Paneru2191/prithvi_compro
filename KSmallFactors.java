cb4602b8ccd3e40d5b7ac69c58a8f7cb51d56bdd
Prithvi Raj Paneru <111098174+Paneru2191@users.noreply.github.com>
2022-12-12 18:46:46 +0545

    Update KSmallFactors.java

--- KSmallFactors.java
+++ KSmallFactors.java
@@ -1,8 +1,8 @@
 public class KSmallFactors {
     public static void main(String[] args) {
-        System.out.println(isKSmall(10,20));
+        System.out.println(hasKSmallFactors(10,20));
     }
-    public static int isKSmall(int k,int n){
+    public static int hasKSmallFactors(int k,int n){
          for(int i=2;i<=n/2;i++){
             if(n%i==0&&i<k&&n/i<k) return 1;
          }
