212630b2fbb7796a450ec20e4c76725b5fc1a23f
Prithvi Raj Paneru <111098174+Paneru2191@users.noreply.github.com>
2022-12-28 21:05:25 +0545

    Add files via upload

---
+++ MeeraArrayEvenAndPrime.java
@@ -0,0 +1,11 @@
+public class MeeraArrayEvenAndPrime {
+    public static void main(String[] args) {
+        int a[]={};
+        System.out.println(isMeeraArray(a));
+    }
+    public static int  isMeeraArray(int[] a){
+        for(int i=0;i<a.length;i++){
+            
+        }
+    }
+}
